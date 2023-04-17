b5403ff9b42c01b44ef1e4bbdffadd7955887696
Rodrigo <zoccorodrigo@gmail.com>
2023-04-17 19:28:03 -0300

    ordena función en módulo correspondiente
    Porque la función manejarResultadosDivisas corresponde al modulo ui.

--- src/ui/general.js
+++ src/ui/general.js
@@ -53,3 +53,23 @@ export function mostrarListadoDivisas(simbolos, simboloBaseSeleccionadoCallback 
     $divisa.onclick = () => simboloBaseSeleccionadoCallback(simboloDivisa,listaDeDivisa);
   })
 };
+
+export async function manejarResultadosDivisas(callbackMonedas = () => {}){
+  const MONTO = Number(document.querySelector("#monto").value.replace(",","."));
+  const DIVISA_BASE = document.querySelector("#divisa-base").dataset.divisa;
+  const DIVISA_CAMBIO = document.querySelector("#divisa-conversion").dataset.divisa;
+  const SPINNER = document.querySelector("#resultado .spinner-border");
+  if(MONTO && DIVISA_BASE && DIVISA_CAMBIO){
+    document.querySelector("#resultado").classList.remove("oculto");
+    if(SPINNER.classList.contains("oculto")){
+      ocultarTextosResultados();
+      SPINNER.classList.remove("oculto");
+
+    }
+    const CAMBIOS = await callbackMonedas(DIVISA_BASE,DIVISA_CAMBIO,MONTO);
+    
+    actualizarTextosResultados(CAMBIOS.divisaBase,CAMBIOS.divisaObjetivo,CAMBIOS.monto,CAMBIOS.montoResultado);
+    
+    SPINNER.classList.add("oculto");
+  }
+};
